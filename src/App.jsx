aae1274e8106e70a44335f3d196fd5b7a02a5584
divyachandrapandi <divya4shivalaya@gmail.com>
2025-05-12 12:28:50 +0530

    Completed FAQ

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
-// import Faq from "./sections/Faq.jsx";
+import Faq from "./sections/Faq.jsx";
 // import Testimonials from "./sections/Testimonials.jsx";
 // import Download from "./sections/Download.jsx";
 // import Footer from "./sections/Footer.jsx";
@@ -14,7 +14,7 @@ const App = () => {
             <Hero />
             <Features />
             <Pricing />
-            {/*<Faq />*/}
+            <Faq />
             {/*<Testimonials />*/}
             {/*<Download />*/}
             {/*<Footer />*/}
