955388cbeabc0cdbcf314c4e91701a68c2e944be
divyachandrapandi <divya4shivalaya@gmail.com>
2025-05-12 12:36:26 +0530

    Completed Testimonial

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
-// import Testimonials from "./sections/Testimonials.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
 // import Download from "./sections/Download.jsx";
 // import Footer from "./sections/Footer.jsx";
 
@@ -15,7 +15,7 @@ const App = () => {
             <Features />
             <Pricing />
             <Faq />
-            {/*<Testimonials />*/}
+            <Testimonials />
             {/*<Download />*/}
             {/*<Footer />*/}
         </main>
